6997d496fcc58256e208745a69d6e6389709ae3b
OxeoH <egor33429@gmail.com>
2022-11-11 01:34:36 +0300

    added filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './Filter.module.scss'
 
 const Filter: React.FC = () => {
   return (
-    <div>Filter</div>
+    <div className={styles.wrapper}>
+      <div className={styles.text}>Filter by: <span className={styles.value}>A - Z / А - Я</span>
+        <div className={styles.popup}>
+          <ul className={styles.sorts}>
+            <li className={styles.sort}>A - Z / А - Я</li>
+            <li className={styles.sort}>Z - A / Я - А</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
